4d91355689553bc59f46278c95cf64dc8d38c4ff
Helbis <filippiotrhelbig@gmail.com>
2020-03-13 14:20:50 +0100

    Added  overloading of [] Deleted unnecessary constructor

--- degree.hpp
+++ degree.hpp
@@ -1,6 +1,12 @@
 #ifndef DEGREE_HPP
 #define DEGREE_HPP 
 
+
+enum MESSAGES : int{
+	NOT_FOUND = -1
+}
+
+
 class Degree{
 	private:
 		std::string name;
@@ -8,7 +14,6 @@ class Degree{
 		std::vector<Student> students;
 
 	public:
-		Degree(void);
 		Degree(int temp_size);
 		
 		//Getters
@@ -18,16 +23,61 @@ class Degree{
 		
 		//Setters
 		void setName(std::string temp);
+
+		void addStudent(std::string temp_name, std::string temp_DNI);
+		int findStudent(std::string temp_DNI);
+
+		Degree operator [](int i);
 };
-
-
-Degree::Degree(void) : N(128){
-	students.reserve(N);
-}
 
 
 Degree::Degree(int temp_size) : N(temp_size){
 	students.reserve(N);
 }
+
+//Getters
+std::string Degree::getName(void){
+	return name;
+}
+
+
+int Degree::getSize(void){
+	return students.size();
+}
+
+
+std::vector<Student> Degree::getStudents(void){
+	return students;
+}
+		
+
+//Setters
+void Degree::setName(std::string temp){
+	name = temp;
+}
+
+
+
+void Degree::addStudent(std::string temp_name, std::string temp_DNI){
+	students.push_back(Student(temp_name, temp_DNI));
+}
+
+
+
+int Degree::findStudent(std::string temp_DNI){
+	for(i=0; i<getSize(); i++){
+		if(students[i].getDNI() == temp_DNI){
+			//Found the student in given degree
+			return i;
+		}
+	}
+	
+	return NOT_FOUND;
+}
+
+Degree operator [](int i){
+	return students[i]
+}
+
 
 #endif
