5e0621463c88fe98c9e7f6c2212bdea366a8c664
Helbis <filippiotrhelbig@gmail.com>
2020-03-13 14:22:13 +0100

    Added searching functionality

--- university.hpp
+++ university.hpp
@@ -7,7 +7,6 @@ class University{
 		std::vector<Degree> degrees;	
 
 	public:
-		University(void);
 		University(int temp);
 
 		//Getters
@@ -16,10 +15,6 @@ class University{
 
 };
 
-
-University::University(void) : N(128){
-	degrees.reserve(N);
-}
 
 University::University(int temp) : N(temp){
 	degrees.reserve(N);
@@ -35,6 +30,26 @@ std::vector<Degree> University::getDegrees(void){
 	return degrees;
 }
 
+
+int University::getTotalNumStudents(void){
+	int total = 0, 
+		sub_end = 0;	
+
+	for(int i=0; i<getSize()-1; i++){	//Go through every degree
+		for(int j=0; j<degrees[i].getSize(); j++){	//Go through every degree's student list
+			
+			//Compare against every other list	
+			for(int k=i+1; k<getSize(); k++){	//Every *next* list of students
+				for(int l=0; l<degrees[k].getSize(); l++){
+					//Compare student from list i, j with k,l
+					if(degrees[i][j] == degrees[k][l])	
+				}
+			}
+		}	
+	}
+	
+	return total - sub_end;
+}
 
 
 #endif
